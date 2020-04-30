289524492f2c13d3e28a94bf94f9f648f8e93820
Stanislav Zinkin <55135260+Zinkin1974@users.noreply.github.com>
2020-05-01 02:07:15 +0300

    Вынес объявления функций в заголовочный файл.

--- MathFunctions.h
+++ MathFunctions.h
@@ -4,8 +4,8 @@ int roundSMI(double);
 double absSMI(double);
 double powSMI(double, double);
 double sqrtSMI(double, double);
-unsigned long long int factSMI(unsigned int); // теперь работает до x = 20 включительно.
-double sinSMI(double, unsigned int); // При x = pi + 2 * pi * k получается число, очень близкое к нулю, но не ноль. Вынес это в отдельный случай.
+unsigned long long int factSMI(unsigned int); // теперь работает до x = 20 включительно
+double sinSMI(double, unsigned int); // При x = pi + 2 * pi * k получается число, очень близкое к нулю, но не ноль. Вынес это в отдельный случай
 double cosSMI(double);
 double tgSMI(double);
 double expSMI(double, unsigned int);
