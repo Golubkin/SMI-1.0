97f01c036e980db7274f4b0dd1bbee76b5c398d9
Stanislav Zinkin <55135260+Zinkin1974@users.noreply.github.com>
2020-05-08 21:11:48 +0300

    Немного изменил функции.
    Убрал параметры по умолчанию у функций, чтобы было легче работать с Bison.
    Убрал в функциях ln и exp обращение к функции pow, чтобы не было ошибок при работе с Bison.

--- MathFunctions.cpp
+++ MathFunctions.cpp
@@ -44,6 +44,75 @@ double SMIabs(double x)
         return -x;
 }
 
+// Возвращает факториал числа x. Для х > 20 не работает.
+unsigned long long int SMIfact(unsigned int x)
+{
+    if (x > 1)
+        return x * SMIfact(x - 1);
+    if (x == 1)
+        return 1;
+    if (x == 0)
+        return 1;
+}
+
+
+// Возвращает ln(x). ln(1 + x) = x - x^2 / 2 + x^3 / 3 - x^4 / 4 + ..., работает для |x| < 1. Остальные х приводим к этому интервалу.
+double SMIln(double x)
+{
+    unsigned int n = 100; // Вынес из параметра по умолчанию для облегчения работы с Bison.
+    if (x >= 0.5 && x <= 2.0)
+    {
+        double res = 0.0;
+        x--;
+        while (n)
+        {
+            const double p = (n % 2 == 0) ? -1.0 : 1.0; // Если n - чётное, то p = -1.0, иначе p = 1.0.
+            double powValue = 1.0; 
+            for(int i = n; i != 0; i--){ powValue *= x; }
+            res += powValue / (p * n);
+            n--;
+        }
+        return res;
+    }
+    if (x > 2.0)
+        return -SMIln(1.0 / x);
+    if (x > 0.0 && x < 0.5)
+        return SMIln(2 * x) + SMIln(0.5);
+}
+
+// Возвращает число е в степени х.
+double SMIexp(double x)
+{
+    unsigned int n = 10;
+    if (SMIabs(x) <= 1.0)
+    {
+        double res = 1.0;
+        while (n)
+        {
+            double powValue = 1.0; 
+            for(int i = n; i != 0; i--){ powValue *= x; }
+            res += powValue / (double)SMIfact(n);
+            n--;
+        }
+        return res;
+    }
+    double e = 2.71828182845904523536;
+    if (x > 1.0)
+    {
+        double int_x = SMIfloor(x), fract_x = x - int_x;
+        double powValue = 1.0;
+        for(int i = int_x; i != 0; i--){ powValue *= e; }
+        return powValue * SMIexp(fract_x);
+    }
+    if (x < -1.0)
+    {
+        double int_x = SMIceil(x), fract_x = x - int_x;
+        double powValue = 1.0;
+        for(int i = int_x; i != 0; i++){ powValue *= e; powValue = 1.0 / powValue; }
+        return powValue * SMIexp(fract_x);
+    }
+}
+
 // Возвращает x в степени n. При целом n число x - любое (кроме случая x = 0 и n <= 0). При действительном n число x >= 0.
 // Иррациональных чисел в C++ нет, поэтому будем вместо действительных рассматривать рациональные
 double SMIpow(double x, double n)
@@ -75,13 +144,14 @@ double SMIpow(double x, double n)
         if (x == 0.0)
             return 0.0;
         if (x > 0.0)
-            return SMIexp(n * SMIln(x, 100), 10); // Почему выдаёт ошибку, если не указывать 100 и 10? Это же аргументы по умолчанию.
+            return SMIexp(n * SMIln(x));
     }
 }
 
 // Возвращает квадратный корень числа x.
-double SMIsqrt(double x, double epsilon = 0.0001)
+double SMIsqrt(double x)
 {
+    double epsilon = 0.0001;
     if (x >= 0)
     {
         double low = 0.0;
@@ -105,20 +175,10 @@ double SMIsqrt(double x, double epsilon = 0.0001)
     }
 }
 
-// Возвращает факториал числа x. Для х > 20 не работает.
-unsigned long long int SMIfact(unsigned int x)
-{
-    if (x > 1)
-        return x * SMIfact(x - 1);
-    if (x == 1)
-        return 1;
-    if (x == 0)
-        return 1;
-}
-
 // Возвращает синус угла х (радианы). n - количество слагаемых в ряде Маклорена.
-double SMIsin(double x, unsigned int n = 10)
+double SMIsin(double x)
 {
+    unsigned int n = 10;
     const double pi = 3.14159265358979323846;
     double res = 0.0;
     int num = n * 2 - 1; // Число, участвующее в последнем члене ряда Маклорена.
@@ -161,56 +221,10 @@ double SMIctg(double x)
     return (double)SMIcos(x) / SMIsin(x);
 }
 
-// Возвращает число е в степени х.
-double SMIexp(double x, unsigned int n = 10)
-{
-    if (SMIabs(x) <= 1.0)
-    {
-        double res = 1.0;
-        while (n)
-        {
-            res += SMIpow(x, n) / (double)SMIfact(n);
-            n--;
-        }
-        return res;
-    }
-    double e = 2.71828182845904523536;
-    if (x > 1.0)
-    {
-        double int_x = floorSMI(x), fract_x = x - int_x;
-        return SMIpow(e, int_x) * SMIexp(fract_x);
-    }
-    if (x < -1.0)
-    {
-        double int_x = SMIceil(x), fract_x = x - int_x;
-        return SMIpow(e, int_x) * SMIexp(fract_x);
-    }
-}
-
-// Возвращает ln(x). ln(1 + x) = x - x^2 / 2 + x^3 / 3 - x^4 / 4 + ..., работает для |x| < 1. Остальные х приводим к этому интервалу.
-double SMIln(double x, unsigned int n = 100)
-{
-    if (x >= 0.5 && x <= 2.0)
-    {
-        double res = 0.0;
-        x--;
-        while (n)
-        {
-            const double p = (n % 2 == 0) ? -1.0 : 1.0; // Если n - чётное, то p = -1.0, иначе p = 1.0.
-            res += SMIpow(x, n) / (p * n);
-            n--;
-        }
-        return res;
-    }
-    if (x > 2.0)
-        return -SMIln(1.0 / x, n);
-    if (x > 0.0 && x < 0.5)
-        return SMIln(2 * x, n) + SMIln(0.5, n);
-}
-
 // Возвращает логарифм по основанию base от аргумента argument.
-double SMIlog(double base, double argument, unsigned int n = 100)
+double SMIlog(double base, double argument)
 {
+    unsigned int n = 100;
     if (base > 0.0 && base != 1.0 && argument > 0.0)
-        return (double)SMIln(argument, n) / SMIln(base, n);
+        return (double)SMIln(argument) / SMIln(base);
 }
