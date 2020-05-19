8b9540d0a899c97b60da605ff05dfcba57a64d22
Nicolay <65ngs@mail.ru>
2020-05-19 13:57:04 +0300

    удалил документацию с ошибками

--- Doxyfile/html/files_dup.js
+++
@@ -1,4 +0,0 @@
-var files_dup =
-[
-    [ "SMI", "dir_2c7e89309f7c31f95c313526dc54d8b1.html", "dir_2c7e89309f7c31f95c313526dc54d8b1" ]
-];
--- Doxyfile/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['mathfunctions_2ecpp_15',['MathFunctions.cpp',['../_math_functions_8cpp.html',1,'']]]
-];
