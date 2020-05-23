f9385e3babd6054a9fb09c0eee331c10d42c398d
Serega-prof <55285397+Serega-prof@users.noreply.github.com>
2020-05-23 11:51:34 +0300

    Тестовый файл
    Необходимые файлы для работы: MathFunvtions.cpp, MathFunctions.h, php.cpp, php.h, package.config

--- tests/test.cpp
+++ tests/test.cpp
@@ -13,21 +13,21 @@
 #include "../MathFunctions/SMItg.cpp"
 #include "../MathFunctions/SMIctg.cpp"
 #include "../MathFunctions/SMIlog.cpp"
-TEST(SMIceil, Vbolst) // Работает нормально
+TEST(SMIceil, Vbolst) // Well done
 {
     ASSERT_EQ(SMIceil(14.9), 15);
     ASSERT_EQ(SMIceil(1134513514), 1134513514);
     ASSERT_EQ(SMIceil(-14.9), -14);
     ASSERT_EQ(SMIceil(-1134513514), -1134513514);
 }
-TEST(SMIfloor, Vmenst) // Работает нормально
+TEST(SMIfloor, Vmenst) // Well done
 {
     ASSERT_EQ(SMIfloor(14.9), 14);
     ASSERT_EQ(SMIfloor(1134513514.9), 1134513514);
     ASSERT_EQ(SMIfloor(-14.9), -15);
     ASSERT_EQ(SMIfloor(-1134513514), -1134513514);
 }
-TEST(SMIabs, Number)// Работает нормально
+TEST(SMIabs, Number)// Well done
 {
     EXPECT_EQ(SMIabs(1), 1);
     EXPECT_EQ(SMIabs(-5), 5);
@@ -37,7 +37,7 @@ TEST(SMIabs, Number)// Работает нормально
     EXPECT_EQ(SMIabs(58989989884134513451345.5254151), 58989989884134513451345.5254151);
     ASSERT_NEAR(SMIabs(32414), 32412 ,2 );
 }
-TEST(SMIround, NumberRound) // Работает нормально
+TEST(SMIround, NumberRound) // Well done
 {
     EXPECT_EQ(SMIround(-2), -2);
     EXPECT_EQ(SMIround(2), 2);
@@ -52,7 +52,7 @@ TEST(SMIround, NumberRound) // Работает нормально
     EXPECT_EQ(SMIround(2.134444444444), 2);
     EXPECT_EQ(SMIround(-249137880.13498), -249137880); 
 }
-TEST(SMIfact, Factorial) // Работает нормально
+TEST(SMIfact, Factorial) // Well done
 {
     EXPECT_EQ(SMIfact(2), 2);
     EXPECT_EQ(SMIfact(0), 1);
@@ -61,7 +61,7 @@ TEST(SMIfact, Factorial) // Работает нормально
     EXPECT_EQ(SMIfact(20), 2432902008176640000);
     EXPECT_EQ(SMIfact(2.8), 2);     
 }
-TEST(SMIln, NaturLog) // Работает нормально 
+TEST(SMIln, NaturLog) // Well done
 {
     ASSERT_NEAR(SMIln(3), 1.09862, 0.00001);
     ASSERT_DOUBLE_EQ(SMIln(1), 0);
@@ -72,7 +72,7 @@ TEST(SMIln, NaturLog) // Работает нормально
     ASSERT_NEAR(SMIln(54135151243435.21433331434614), 31.62250, 0.00001);
 }
 
-TEST(SMIexp, Exponenta) // Работает нормально
+TEST(SMIexp, Exponenta) // Well done
 {
     ASSERT_NEAR(SMIexp(0), 1, 0.1);
     ASSERT_NEAR(SMIexp(1), 2.71828, 0.00001);
@@ -83,7 +83,7 @@ TEST(SMIexp, Exponenta) // Работает нормально
     ASSERT_NEAR(SMIexp(3.311324732), 27.4214, 0.0001);
     ASSERT_NEAR(SMIexp(20), 485165000, 1000); 
 }
-TEST(SMIpow, XvStepenyN) // Работает нормально
+TEST(SMIpow, XvStepenyN) // Well done
 {  
    ASSERT_EQ(SMIpow(0, 1234), 0);
    ASSERT_EQ(SMIpow(1, 1), 1);
@@ -104,7 +104,7 @@ TEST(SMIpow, XvStepenyN) // Работает нормально
    ASSERT_NEAR(SMIpow(3.34, 3), 37.2597, 0.0001);
    ASSERT_NEAR(SMIpow(3.34, 3.3214241245), 54.9015, 0.0001);
 }
-TEST(SMIsqrt, sqrt) // Работает нормально 
+TEST(SMIsqrt, sqrt) // Well done
 {
    ASSERT_NEAR(SMIsqrt(0), 0, 0.01); 
    ASSERT_NEAR(SMIsqrt(1), 1, 0.01); 
@@ -115,7 +115,7 @@ TEST(SMIsqrt, sqrt) // Работает нормально
    ASSERT_NEAR(SMIsqrt(312353568673), 558886.00687, 0.00001);  
    ASSERT_NEAR(SMIsqrt(312353568673.5622256465), 558886.00687, 0.00001); 
 }
-TEST(SMIsin, sinus) // Работает нормально
+TEST(SMIsin, sinus) // Well done
 {
     ASSERT_EQ(SMIsin(0), 0);
     ASSERT_NEAR(SMIsin(3.1415), 0.0000926535 , 0.000000001); 
@@ -123,27 +123,27 @@ TEST(SMIsin, sinus) // Работает нормально
     ASSERT_NEAR(SMIsin(-2), -0.909297, 0.000001 );
     ASSERT_NEAR(SMIsin(-1453145.144435), -0.994143, 0.000001);
 }
-TEST(SMIcos, cosinus) // Работает нормально
+TEST(SMIcos, cosinus) // Well done
 {
     ASSERT_NEAR(SMIcos(0), 1, 0.00000001);
     ASSERT_NEAR(SMIcos(1), 0.540302, 0.000001);
     ASSERT_NEAR(SMIcos(-2), -0.416146, 0.000001);
     ASSERT_NEAR(SMIcos(-1453145.144435), 0.108067, 0.00001); 
 }
-TEST(SMItg, tangens) // Работает нормально
+TEST(SMItg, tangens) // Well done
 {
     ASSERT_EQ(SMItg(0), 0);
     ASSERT_NEAR(SMItg(1), 1.55740, 0.00001);
     ASSERT_NEAR(SMItg(-2), 2.18503, 0.00001);
     ASSERT_NEAR(SMItg(12345.12345), -4.17452, 0.0001);
 }
-TEST(SMIctg, catanges) // Работает нормально
+TEST(SMIctg, catanges) // Well done
 {
     ASSERT_NEAR(SMIctg(1), 0.64209, 0.00001);
     ASSERT_NEAR(SMIctg(-2), 0.45765, 0.00001);
     ASSERT_NEAR(SMIctg(12345.12345), -0.23954, 0.0001);
 }
-TEST(SMIlog, logorifm)
+TEST(SMIlog, logorifm) // Well done
 {
     ASSERT_EQ(SMIlog(2, 1), 0);
     ASSERT_EQ(SMIlog(2, 2), 1);
